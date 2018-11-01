907a17bd05c2c86737561d9c6c6ed3f318e31923
funkycochise <funkycochise@free.fr>
2018-11-01 14:12:56 +0100

    jammaSD key mapping, Release

--- Arcade-Galaga.sv
+++ Arcade-Galaga.sv
@@ -164,6 +164,12 @@ always @(posedge clk_sys) begin
 
 			'h005: btn_one_player  <= pressed; // F1
 			'h006: btn_two_players <= pressed; // F2
+			'h004: btn_coin        <= pressed; // F3
+
+			'h016: btn_one_player  <= pressed; // 1
+			'h01e: btn_two_players <= pressed; // 2
+			'h02e: btn_coin        <= pressed; // 5 coin		
+			
 		endcase
 	end
 end
@@ -175,6 +181,7 @@ reg btn_left  = 0;
 reg btn_fire  = 0;
 reg btn_one_player  = 0;
 reg btn_two_players = 0;
+reg btn_coin = 0;
 
 wire m_left   = status[2] ? btn_down  | joy[2] : btn_left  | joy[1];
 wire m_right  = status[2] ? btn_up    | joy[3] : btn_right | joy[0];
@@ -182,7 +189,7 @@ wire m_fire   = btn_fire | joy[4];
 
 wire m_start1 = btn_one_player  | joy[5];
 wire m_start2 = btn_two_players | joy[6];
-wire m_coin   = m_start1 | m_start2;
+wire m_coin   = btn_coin;
 
 wire hblank, vblank;
 wire ce_vid = 1;
